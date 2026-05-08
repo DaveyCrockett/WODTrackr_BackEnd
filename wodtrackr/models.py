24d0d66b068328afacd3c5f0f23a8a1325b298fe
DaveyCrockett <67625797+DaveyCrockett@users.noreply.github.com>
2026-05-08 09:57:31 -0700

    fix: Correct choice value for difficulty field in ExerciseProgram model

--- wodtrackr/models.py
+++ wodtrackr/models.py
@@ -148,7 +148,7 @@ class ExerciseProgram(models.Model):
 		('beginner', 'Beginner'),
 		('intermediate', 'Intermediate'),
 		('advanced', 'Advanced'),
-		('all levels', 'All Levels'),
+		('all_levels', 'All Levels'),
 	]
 
 	DURATION_WEEKS_CHOICES = [
